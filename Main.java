52c00a614685a3dd090711eef8218772f6f7cd93
blue-94 <jtenn84@gmail.com>
2024-11-04 20:36:12 +0000

    Adds Comments to count8

--- Main.java
+++ Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static int count8(int n) {
         int count = 0;
 
+        // If rightmost digit is 8, add 1
         if (n % 10 == 8) {
             count++;
-        }
-
-        if (n % 100 == 88) {
-            count++;;
+            // If the secondmost to the right is 8 add 1
+            if (n % 100 == 88) {
+                count++;;
+            }
         }
     
         // Base case: if there is 1 digit return
@@ -18,7 +19,9 @@ public class Main {
         }
 
         else {
+            // Count amount of 8s in the numbers to the left of the rightmost number and add it to count
             count += count8(n / 10);
+            // Return count 
             return count;
         }
     }
